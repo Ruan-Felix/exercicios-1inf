0f367e7f2b78f7811c4f943a0c267d5f46aa179d
Ruan Felix <felixruan991@gmail.com>
2026-09-11 14:16:30 +0000

    terminei

--- 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
+++ 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
@@ -1,21 +1,22 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
-const cliente = "Mariana Silva";
-const cidade = "Fortaleza";
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
 
-const produto = "Notebook Pro";
-const categoria = "Notebook";
-const preco = 3500;
-const quantidade = 2;
-const descontoPercentual = 10;
+const produto = "Notebook Pro"
+const categoria = "Notebook"
+const preco = 3500
+const quantidade = 2
+const descontoPercentual = 10
 
-const valorPago = 7000;
+const valorPago = 7000
 
-const subtotal = preco * quantidade;
+const subtotal = preco * quantidade
 const valorDesconto = (subtotal * descontoPercentual) / 100;
-const valorFinal = subtotal - valorDesconto;
-const troco = valorPago - valorFinal;
+const valorFinal = subtotal - valorDesconto
+const troco = valorPago - valorFinal
 
-const resumo = `==================================================
+const resumo = `
+==================================================
 TECHSTORE SISTEMAS
 RESUMO DO PEDIDO
 ==================================================
@@ -37,9 +38,9 @@ Valor pago: R$ ${valorPago}
 Troco: R$ ${troco}
 
 Obrigado pela compra!
-==================================================`;
+==================================================`
 
-console.log(resumo);
+console.log(resumo)
 
 
 
